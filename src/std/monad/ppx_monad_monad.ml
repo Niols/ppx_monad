3c1246dc07deef359f4d138d10d8aeaa47d6c930
Niols <niols@niols.fr>
2022-02-09 21:23:12 +0100

    add monad monad to std

---
+++ src/std/monad/ppx_monad_monad.ml
@@ -0,0 +1,18 @@
+open Ppxlib
+
+let mk_return ~loc x =
+  [%expr return [%e x]]
+
+let mk_bind ~loc e f =
+  [%expr bind [%e e] [%e f]]
+
+let mk_fail ~loc x =
+  [%expr fail [%e x]]
+
+let mk_catch ~loc e f =
+  [%expr catch [%e e] [%e f]]
+
+let () = Ppx_monad.register "monad"
+    ~applies_on:"m|monad"
+    ~mk_return ~mk_bind
+    ~mk_fail ~mk_catch
